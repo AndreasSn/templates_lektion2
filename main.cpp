a3db2bd4e6de639839b34c930386234980be9bad
andreas <androgles1994@gmail.com>
2019-09-03 13:33:49 +0200

    pis

--- main.cpp
+++ main.cpp
@@ -13,10 +13,22 @@ int main(){
     myArray<int, 3> a1 = myArray<int, 3>();
 
     a1.fill(5);
+    a1[2] = 159;
 
-    a1[0] = 3; // Assuming that 'my' has been appropriately allocated based on MyArray.
-    int number = 10;
-    std ::cout << "Looking for '3'? " << endl << (myFind(a1.begin(), a1.end(), number) != a1.end() ? "found" : "sry no") << std ::endl;
+    myArray<double, 3> a2;
+     a2 = a1;
+
+    // a2[1] = a1[2];
+    // a2 = a1[2]
+    a1.print();
+    a2.print();
+
+
+    // a1.fill(5);
+
+    //a1[0] = 3.3; // Assuming that 'my' has been appropriately allocated based on MyArray.
+    //int number = 3;
+    //std ::cout << "Looking for '3'? " << endl << (myFind(a1.begin(), a1.end(), number) != a1.end() ? "found" : "sry no") << std ::endl;
 
 }
 
--- myArray.hpp
+++ myArray.hpp
@@ -10,12 +10,45 @@ public:
     myArray(){};
     ~myArray(){};
 
+    //copy constructor
+
+    template <typename T2, size_t sidid>
+    myArray( myArray<T2, sidid>& ar1)
+    {
+         for (auto i = 0; i < arraySize; i++)
+         {
+            array_[i] = ar1[i];
+         }  
+    }
+
+    myArray &operator=(myArray const &other)
+    {
+        // 1: allocate new memory and copy the elements
+
+        if (this != &other)
+        {
+
+            cout << endl <<  "size:" << other.getSize() << endl;
+            array_ = new T[other.getSize()];
+        }
+        // 2: deallocate old memory
+
+        // 3: assign the new memory to the object
+    };
+
     void fill(const T& t){
-        for (size_t i = 0; i < size(); i++)
+        for (size_t i = 0; i < getSize(); i++)
         {
             array_[i] = t;
         }
     };
+
+    void print(){
+        for (int i = 0; i < getSize(); i++)
+        {
+            cout << "element: " << i  << " is : "<< array_[i] << endl;
+        } 
+    }
 
     T* begin(){
         return &array_[0];
@@ -25,20 +58,21 @@ public:
          return &array_[arraySize];
     };
 
-     T &operator[](size_t index)
+    T &operator[](size_t index)
     {
-        if (index >= size())
+        if (index >= getSize())
         {
             cout << "Array index out of bound, exiting";
             throw std::out_of_range ("out of range");
         }
         return array_[index];
     };
-    const size_t size(){return arraySize;};
+
+    size_t getSize() {return arraySize;};
 };
 
-template <typename T>
-T *myFind(T *first, T *last, T &v)
+template <typename T, typename T1>
+T *myFind(T *first, T *last, T1 &v)
 {
     for (auto i = first; i < last; i++)
     {
